065b03147a0471803beefc4be77c20f7205533bb
RudraPratap-CodeRunner <98970737+RudraPratap-CodeRunner@users.noreply.github.com>
2024-05-03 17:52:40 +0530

    cart and place order page added

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,12 +1,15 @@
-import React, { useContext } from 'react'
-import './Cart.css'
-import { StoreContext } from '../../context/StoreContext'
+import React, { useContext } from "react";
+import "./Cart.css";
+import { StoreContext } from "../../context/StoreContext";
+import { useNavigate } from "react-router-dom";
 const Cart = () => {
+  const { cartItems, food_list, removeFromCart, getTotalCartAmount } =
+    useContext(StoreContext);
 
-  const {cartItems,food_list,removeFromCart} = useContext(StoreContext);
+  const navigate = useNavigate();  
 
   return (
-     <div className="cart">
+    <div className="cart">
       <div className="cart-items">
         <div className="cart-items-title">
           <p>Items</p>
@@ -18,27 +21,60 @@ const Cart = () => {
         </div>
         <br />
         <hr />
-        {food_list.map((item,index)=>{
-          if(cartItems[item._id]>0)
-          {
+        {food_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
             return (
               <div>
                 <div className="cart-items-title cart-items-item">
-                <img src={item.image} alt="" />
-                <p>{item.name}</p>
-                <p>${item.price}</p>
-                <p>{cartItems[item._id]}</p>
-                <p>${item.price*cartItems[item._id]}</p>
-                <p className='cross' onClick={()=>removeFromCart(item._id)} >X</p>
+                  <img src={item.image} alt="" />
+                  <p>{item.name}</p>
+                  <p>${item.price}</p>
+                  <p>{cartItems[item._id]}</p>
+                  <p>${item.price * cartItems[item._id]}</p>
+                  <p className="cross" onClick={() => removeFromCart(item._id)}>
+                    X
+                  </p>
+                </div>
+                <hr />
               </div>
-              <hr />
-              </div>
-            )
+            );
           }
         })}
       </div>
-     </div>
-  )
-}
+      <div className="cart-bottom">
+        <div className="cart-total">
+          <h2>Cart Total</h2>
+          <div>
+            <div className="cart-total-details">
+              <p>Subtotal</p>
+              <p>${getTotalCartAmount()}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <p>Delivery Fee</p>
+              <p>${getTotalCartAmount()===0?0:2}</p>
+            </div>
+            <hr />
+            <div className="cart-total-details">
+              <b>Total</b>
+              <b>${getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+            </div>
+            <hr />
+          </div>
+          <button onClick={()=>navigate('/order')} >Proceed To Checkout</button>
+        </div>
+        <div className="cart-promocode">
+          <div>
+            <p>If you have a promo code, Enter it here</p>
+            <div className="cart-promocode-input">
+              <input type="text" name="" id="" placeholder="promo code" />
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Cart
+export default Cart;
