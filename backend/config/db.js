54495873bb7d9c5c120658d5a7b9eec17435bf12
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-08-10 10:47:18 +0530

    backend changes

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,23 +1,8 @@
 import mongoose from "mongoose";
-import 'dotenv/config'
-// export const connectDB = async () => {
-//   await mongoose
-//     .connect(
-//       "mongodb+srv://rudrapratapsingh:ruchi2011@cluster0.splz07z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/project1"
-//     )
-//     .then(() => {
-//       console.log("connected to tomato");
-//     });
-// };
+import "dotenv/config";
 
 export const connectDB = async () => {
-    await mongoose
-      .connect(
-        process.env.MONGODB_KEY
-      )
-      .then(() => {
-        console.log("connected to tomato");
-      });
-  };
-
-
+  await mongoose.connect(process.env.MONGODB_KEY).then(() => {
+    console.log("connected to tomato");
+  });
+};
