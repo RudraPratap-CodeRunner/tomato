46b9d997ba7785c9c90879ca7eeabb2703b5a528
RudraPratap-CodeRunner <rp.rudrapratapofficial@gmail.com>
2024-09-11 00:05:13 +0530

    syntax error removed for importing axios

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ import foodRouter from "./routes/foodRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
 import orderRouter from "./routes/orderRoute.js"
-import axios from axios
+import axios from 'axios'
 
 
 const app = express()
